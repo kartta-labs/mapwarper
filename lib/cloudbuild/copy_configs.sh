18653f5a01b8cad8fada0e3a5b78ee4e9f95adf5
Tim Waters <chippy2005@gmail.com>
2019-07-01 17:38:50 +0100

    add simple copy config bash script for use with cloud build

---
+++ lib/cloudbuild/copy_configs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -n config/database.example.yml config/database.yml 
+cp -n config/secrets.yml.example config/secrets.yml 
+cp -n config/application.example.yml config/application.yml
