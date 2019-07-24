edd8c5c3d204612b88856a9e6f3bbb51fa9ce041
Tim Waters <chippy2005@gmail.com>
2019-07-24 12:30:00 +0100

    update and fix maps job task to use the new ocr parsing code

--- test/jobs/maps_ocr_job_test.rb
+++ test/jobs/maps_ocr_job_test.rb
@@ -7,7 +7,8 @@ Rails.application.config.active_job.queue_adapter = :inline
 class MapsOcrJobTest < ActiveJob::TestCase
   setup do
     @map =  FactoryGirl.create(:warped_map)
-    MapsOcrJob.any_instance.stubs(:google_image_annotate).returns( JSON.parse({responses: [ text_annotations: [ description: "FOOBAR"]]}.to_json, object_class: OpenStruct) )
+
+    MapsOcrJob.any_instance.stubs(:google_image_annotate).returns( JSON.parse( {"responses":[{"full_text_annotation":{"pages":[{"blocks":[{"paragraphs":[{"words":[{"symbols":[{"text":"F"},{"text":"O"},{"text":"O"},{"text":"B"},{"text":"A"},{"text":"R"}]}],"confidence":0.99}]}]}]}}]}.to_json, object_class: OpenStruct) )
     MapsOcrJob.any_instance.stubs(:call_google_geocode).returns("dummy text")
 
   end
@@ -15,6 +16,7 @@ class MapsOcrJobTest < ActiveJob::TestCase
   test 'that map gets an ocr result' do
     MapsOcrJob.perform_now(@map)
     assert_not @map.reload.ocr_result.blank?
+    assert_equal "foobar",  @map.reload.ocr_result
   end
 
   test 'the job gets scheduled' do
@@ -23,3 +25,50 @@ class MapsOcrJobTest < ActiveJob::TestCase
     end
   end
 end
+
+# stripped down version of json response. Note that the Ruby client converts fullTextAnnotation to full_text_annotation
+# {
+#   "responses": [
+#     {
+#       "fullTextAnnotation": {
+#         "pages": [
+#           {
+#             "blocks": [
+#               {
+#                 "paragraphs": [
+#                   {
+#                     "words": [
+#                       {
+#                         "symbols": [
+#                           {
+#                             "text": "F"
+#                           },
+#                           {
+#                             "text": "O"
+#                           },
+#                           {
+#                             "text": "O"
+#                           },
+#                           {
+#                             "text": "B"
+#                           },
+#                           {
+#                             "text": "A"
+#                           },
+#                           {
+#                             "text": "R"
+#                           }
+#                         ]
+#                       }
+#                     ],
+#                     "confidence": 0.99
+#                   }
+#                 ]
+#               }
+#             ]
+#           }
+#         ]
+#       }
+#     }
+#   ]
+# }
